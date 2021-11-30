a117273b4da4bede4d69d05832481e2c505f4794
James Frost <james@jamesfrost.co.uk>
2021-11-30 13:43:21 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutomatticAbout
+
+A description of this package.
---
+++ Sources/AutomatticAbout/AutomatticAbout.swift
@@ -0,0 +1,6 @@
+public struct AutomatticAbout {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
