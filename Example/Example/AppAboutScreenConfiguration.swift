83238f09c5b436e5b8d7ec95fef3b8a4db9f8674
James Frost <james@jamesfrost.co.uk>
2021-12-13 21:17:49 +0000

    Extract strings to constants

--- Example/Example/AppAboutScreenConfiguration.swift
+++ Example/Example/AppAboutScreenConfiguration.swift
@@ -6,28 +6,28 @@ class AppAboutScreenConfiguration: AboutScreenConfiguration {
     var sections: [AboutScreenSection] {
         [
             [
-                AboutItem(title: "Rate Us"),
-                AboutItem(title: "Blog", subtitle: "blog.wordpress.com", cellStyle: .value1, action: { [weak self] context in
-                    self?.present(url: URL(string: "https://blog.wordpress.com")!, from: context.viewController)
+                AboutItem(title: Titles.rateUs),
+                AboutItem(title: Titles.twitter, subtitle: Subtitles.twitter, cellStyle: .value1, action: { [weak self] context in
+                    self?.present(url: Links.twitter, from: context.viewController)
                 }),
-                AboutItem(title: "Twitter", subtitle: "@WordPressiOS", cellStyle: .value1, action: { [weak self] context in
-                    self?.present(url: URL(string: "https://blog.wordpress.com")!, from: context.viewController)
+                AboutItem(title: Titles.blog, subtitle: Subtitles.blog, cellStyle: .value1, action: { [weak self] context in
+                    self?.present(url: Links.blog, from: context.viewController)
                 })
             ],
             [
-                AboutItem(title: "Legal and More", accessoryType: .disclosureIndicator, action: { context in
-                    context.showSubmenu(title: "Legal and More", configuration: LegalAndMoreSubmenuConfiguration())
+                AboutItem(title: Titles.legalAndMore, accessoryType: .disclosureIndicator, action: { context in
+                    context.showSubmenu(title: Titles.legalAndMore, configuration: LegalAndMoreSubmenuConfiguration())
                 })
             ],
             [
-                AboutItem(title: "Our Apps", accessoryType: .disclosureIndicator, hidesSeparator: true, action: { [weak self] context in
-                    self?.present(url: URL(string: "https://automattic.com")!, from: context.viewController)
+                AboutItem(title: Titles.automatticFamily, accessoryType: .disclosureIndicator, hidesSeparator: true, action: { [weak self] context in
+                    self?.present(url: Links.automatticFamily, from: context.viewController)
                 }),
                 AboutItem(title: "", cellStyle: .appLogos)
             ],
             [
-                AboutItem(title: "Work With Us", subtitle: "Join From Anywhere", cellStyle: .subtitle, accessoryType: .disclosureIndicator, action: { [weak self] context in
-                    self?.present(url: URL(string: "https://automattic.com/work-with-us")!, from: context.viewController)
+                AboutItem(title: Titles.workWithUs, subtitle: Subtitles.workWithUs, cellStyle: .subtitle, accessoryType: .disclosureIndicator, action: { [weak self] context in
+                    self?.present(url: Links.workWithUs, from: context.viewController)
                 })
             ]
         ]
@@ -47,7 +47,6 @@ class AppAboutScreenConfiguration: AboutScreenConfiguration {
     func didHide(viewController: UIViewController) {}
 
     // Provides app info to display in the header of the about screen.
-    //
     static var appInfo: AboutScreenAppInfo {
         let name = (Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String) ?? ""
         let version = (Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String) ?? ""
@@ -55,6 +54,29 @@ class AppAboutScreenConfiguration: AboutScreenConfiguration {
         return AboutScreenAppInfo(name: name,
                                   version: "Version \(version)",
                                   icon: UIImage(named: "AppIcon")!)
+    }
+
+    private enum Titles {
+        static let rateUs           = NSLocalizedString("Rate us", comment: "Title of button that allows the user to rate the app in the App Store")
+        static let twitter          = NSLocalizedString("Twitter", comment: "Title of a button linking to the app's Twitter profile")
+        static let blog             = NSLocalizedString("Blog", comment: "Title of a button linking to the app's blog")
+        static let legalAndMore     = NSLocalizedString("Legal and more", comment: "Title of a button linking to a list of legal documents like privacy policy, terms of service, etc")
+        static let automatticFamily = NSLocalizedString("Automattic family", comment: "Title of a button linking to the Automattic website")
+        static let workWithUs       = NSLocalizedString("Work with us", comment: "Title of a button linking to the Automattic Work With Us web page")
+    }
+
+    private enum Subtitles {
+        static let twitter          = "@WordPressiOS"
+        static let blog             = "blog.wordpress.com"
+        static let workWithUs       = NSLocalizedString("Join from anywhere", comment: "Subtitle for button displaying the Automattic Work With Us web page, indicating that Automattic employees can work from anywhere in the world")
+    }
+
+    private enum Links {
+        static let rateUs           = URL(string: "https://itunes.apple.com/app/id335703880?mt=8&action=write-review")!
+        static let twitter          = URL(string: "https://twitter.com/WordPressiOS")!
+        static let blog             = URL(string: "https://blog.wordpress.com")!
+        static let automatticFamily = URL(string: "https://automattic.com")!
+        static let workWithUs       = URL(string: "https://automattic.com/work-with-us")!
     }
 }
 
